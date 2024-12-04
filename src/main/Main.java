17082ddb0cdd15ad57a76bac7307e11eb9ce4562
manudc12 <manuel.dopico.caneiro@udc.es>
2024-12-04 19:47:11 +0100

    added code (changed the function)

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,18 +5,23 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the first number:");
-        int num1 = scanner.nextInt();
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
 
-        System.out.println("Enter the second number:");
-        int num2 = scanner.nextInt();
+        int[] numbers = new int[n];
 
-        System.out.println("Enter the third number:");
-        int num3 = scanner.nextInt();
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
 
-        int sum = num1 + num2 + num3;
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
 
-        System.out.println("The sum of the three numbers is: " + sum)
-
+        System.out.println("The sum of the array numbers is: " + sum);
+        
         scanner.close();
     }
+}
